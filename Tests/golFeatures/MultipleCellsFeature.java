d74043033feee947a31fb3c0c5d4bfb9e9ef1a82
Hari <lkhari@gmail.com>
2013-06-28 17:06:27 +0100

    Added a new test, MultipleCellsFeature, which made me realise i edit on the state i work read from. So fixed that problem.

---
+++ Tests/golFeatures/MultipleCellsFeature.java
@@ -0,0 +1,32 @@
+package golFeatures;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import gol.Game;
+
+public class MultipleCellsFeature {
+
+	@Test
+	public void sparseGridWithNoSurvivors() {
+
+		//given
+		boolean[][] board= new boolean[5][5];
+		
+		board[1][1]=true;
+		board[1][3]=true;
+		board[3][1]=true;
+		board[3][3]=true;
+		
+		Game game= new Game(board);
+		//when
+		game.evolve();
+		
+		//then
+		assertFalse("Cell 1,1 should have died", game.isCellAlive(1, 1));
+		assertFalse("Cell 1,3 should have died", game.isCellAlive(1, 3));
+		assertFalse("Cell 3,1 should have died", game.isCellAlive(3, 1));
+		assertFalse("Cell 3,3 should have died", game.isCellAlive(3, 3));
+	}
+
+}
--- src/gol/Game.java
+++ src/gol/Game.java
@@ -2,13 +2,19 @@ package gol;
 
 public class Game {
 	private boolean[][] board;
+	private boolean[][] prevBoardState;
 
 	public Game(boolean[][] board) {
-		this.board=board;
+		this.board = board;
 	}
 
 	public void evolve() {
-				board[1][1] = updateCellLife(1, 1);
+		prevBoardState = currentState();
+		for (int j = 0; j < board.length; j++) {
+			for (int i = 0; i < board[0].length; i++) {
+				board[i][j] = updateCellLife(i, j);
+			}
+		}
 	}
 
 	private boolean updateCellLife(int i, int j) {
@@ -19,17 +25,34 @@ public class Game {
 		}
 		return amountOfNeighbours == 3;
 	}
-	
+
 	public int amountOfLiveNeighbours(int x, int y) {
 		int liveNeighbours = 0;
-		for (int j = 0; j < board.length; j++) {
-			for (int i =0; i < board[0].length; i++) {
-					liveNeighbours += board[i][j] ? 1 : 0;
+		for (int i = x - 1; i < x + 2; i++) {
+			for (int j = y - 1; j < y + 2; j++) {
+				if (outOfBounds(i, j)) {
+					liveNeighbours += prevBoardState[i][j] ? 1 : 0;
+				}
 			}
 		}
 		return liveNeighbours;
 	}
-	
+
+	public boolean outOfBounds(int i, int j) {
+		if (j < board.length && j >= 0)
+			return i < board.length && i >= 0;
+		return false;
+	}
+
+	public boolean[][] currentState() {
+		boolean[][] newArray = (boolean[][]) board.clone();
+
+		for (int row = 0; row < board.length; row++) {
+			newArray[row] = (boolean[]) board[row].clone();
+		}
+		return newArray;
+	}
+
 	public boolean isCellAlive(int i, int j) {
 		return board[i][j];
 	}
