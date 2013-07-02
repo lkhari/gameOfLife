7e887fd795615a8e1e3ca94eeee04b63b2f8cd67
Hari <lkhari@gmail.com>
2013-07-02 11:18:41 +0100

    Refactoring

--- src/gol/Game.java
+++ src/gol/Game.java
@@ -12,21 +12,21 @@ public class Game {
 		prevBoardState = currentState();
 		for (int j = 0; j < board.length; j++) {
 			for (int i = 0; i < board[0].length; i++) {
-				board[i][j] = updateCellLife(i, j);
+				board[i][j] = isCellStillLife(i, j);
 			}
 		}
 	}
 
-	private boolean updateCellLife(int i, int j) {
-		int amountOfNeighbours = amountOfLiveNeighbours(i, j);
-		if (board[i][j]) {
+	private boolean isCellStillLife(int x, int y) {
+		int amountOfNeighbours = amountOfLiveNeighbours(x, y);
+		if (board[x][y]) {
 			amountOfNeighbours--;
 			return amountOfNeighbours >= 2 && amountOfNeighbours <= 3;
 		}
 		return amountOfNeighbours == 3;
 	}
 
-	public int amountOfLiveNeighbours(int x, int y) {
+	private int amountOfLiveNeighbours(int x, int y) {
 		int liveNeighbours = 0;
 		for (int i = x - 1; i < x + 2; i++) {
 			for (int j = y - 1; j < y + 2; j++) {
@@ -38,13 +38,13 @@ public class Game {
 		return liveNeighbours;
 	}
 
-	public boolean outOfBounds(int i, int j) {
-		if (j < board.length && j >= 0)
-			return i < board.length && i >= 0;
+	private boolean outOfBounds(int x, int y) {
+		if (y < board.length && y >= 0)
+			return x < board.length && x >= 0;
 		return false;
 	}
 
-	public boolean[][] currentState() {
+	private boolean[][] currentState() {
 		boolean[][] newArray = (boolean[][]) board.clone();
 
 		for (int row = 0; row < board.length; row++) {
@@ -53,8 +53,7 @@ public class Game {
 		return newArray;
 	}
 
-	public boolean isCellAlive(int i, int j) {
-		return board[i][j];
+	public boolean isCellAlive(int x, int y) {
+		return board[x][y];
 	}
-
 }
